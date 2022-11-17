a10bbde0a66d938d32f4e8461dd367090f431a95
mxxikr <mxxikr@gmail.com>
2022-11-18 01:40:59 +0900

    자바의 정석 Chapter 1

---
+++ learning/java_standard/src/java_standard/Hello.java
@@ -0,0 +1,16 @@
+package java_standard;
+
+/*
+ Date : 2022.11.18
+ Source : Hello.java
+ Author : mxxikr
+ Email : mxxikr@gmail.com 
+ */
+
+class Hello {
+	public static void main(String[] args) { /* 프로그램의 시작*/
+		System.out.println("Hello, world."); // 화면에 글자 출력
+		System.out.println("Hello, /* 이건 주석 아님 */ world."); 
+		System.out.println("Hello, world. // 이것도 주석 아님"); 
+	}
+}
